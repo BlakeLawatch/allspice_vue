f8a81d584db7e64eb50a8b8c1a43506523587415
Blake Lawatch <blakelawatch@gmail.com>
2023-12-30 19:52:17 -0700

    weather functional

--- client/src/components/QuoteComponent.vue
+++ client/src/components/QuoteComponent.vue
@@ -2,8 +2,8 @@
     <div v-if="quote" class="col-12">
         <section class="row justify-content-center">
             <div class="col-10 box-shadow text-center p-2 text-light">
-                <p class="fs-2 mb-0">{{ quote.author }} </p>
-                <p class="fs-2 mb-0">{{ quote.content }} </p>
+                <p class="fs-2 mb-0 quote-body">{{ quote.content }} </p>
+                <p class="fs-2 mb-0 quote-author">{{ quote.author }} </p>
             </div>
         </section>
     </div>
@@ -23,4 +23,19 @@ export default {
 </script>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.quote-body {
+    cursor: pointer;
+}
+
+.quote-body:hover+.quote-author {
+    visibility: visible;
+    opacity: 1;
+    transition: 1s;
+}
+
+.quote-author {
+    visibility: hidden;
+    opacity: 0;
+}
+</style>
